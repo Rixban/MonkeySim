b5be7805057a4719eca534d339d4c6e2d5112c3e
Rixban <ConanEtigawa24@aol.com>
2016-11-06 15:08:52 -0500

    Method refactor, no pinning tests

--- MonkeySim.java
+++ MonkeySim.java
@@ -55,20 +55,7 @@ public class MonkeySim {
      */
     
     public static Monkey getFirstMonkey(List<Monkey> ml) {
-
-	int x = ml.size() - 1;
-	int f = x * 33;
-	int r = 17;
-	for (int j = x; j >= 0; j--) {
-	    if (ml.get(j).getMonkeyNum() == 1 && f > 0) {
-		f--;
-		j = x;
-	    } else if (ml.get(j).getMonkeyNum() == 1 && f == 0) {
-		return ml.get(j);
-	    } 
-	}
-	
-	return null;
+    	return ml.get(1);
     }
 
     /**
@@ -82,20 +69,12 @@ public class MonkeySim {
     public static String stringifyResults(int c, Monkey m, Monkey m2) {
 	String toReturn = new String("");
 	try {
-	    for (int j=0; j < HEADER; j++) {
-		toReturn += "@";
-	    }
-	    toReturn += new String("//Round ");
-	    toReturn += new String("" + c);
-	    toReturn += new String(": Threw banana from Monkey (#");
-	    toReturn += new String(m.getMonkeyNum() + " / ID " + m.getId());
-	    toReturn += new String(") to Monkey (#");
-	    toReturn += new String(m2.getMonkeyNum() + " / ID " + m2.getId() + ")");
+	    toReturn += new String("//Round " + c+": Threw banana from Monkey (#"+m.getMonkeyNum() + " / ID " + m.getId()+") to Monkey (#"+m2.getMonkeyNum() + " / ID " + m2.getId() + ")");
 	} catch (NoIdException noidex) {
 	    System.out.println("INVALID MONKEY!");
 	    System.exit(2);
 	}
-	return toReturn.substring(HEADER);
+	return toReturn;
     }
     
     /**
